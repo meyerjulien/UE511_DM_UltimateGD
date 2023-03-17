2f9bfa86c00a9b287bcd4641bff1381a3956f64c
Ju <jumey31@gmail.com>
2023-03-17 18:22:56 +0100

    Started working on AnimInstance feature.

---
+++ Source/UE511_DM_UltimateGD/Public/Characters/EchoAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EchoAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE511_DM_ULTIMATEGD_API UEchoAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
