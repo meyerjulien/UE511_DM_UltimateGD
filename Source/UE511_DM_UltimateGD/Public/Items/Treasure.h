df0b637b0b0ff4e956265ed4d9973da97f552567
Ju <jumey31@gmail.com>
2023-04-28 16:55:35 +0200

    Added child class Treasure from Item class.

---
+++ Source/UE511_DM_UltimateGD/Public/Items/Treasure.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Treasure.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE511_DM_ULTIMATEGD_API ATreasure : public AItem
+{
+	GENERATED_BODY()
+	
+};
